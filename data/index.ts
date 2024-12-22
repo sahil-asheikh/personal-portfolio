3b6b0a3f5b88516532862dc800c6172a43c861f3
Sahil Sheikh <sahilsheikh.dev@gmail.com>
2024-12-22 23:21:55 +0530

    migrated from react to next

---
+++ data/index.ts
@@ -0,0 +1,218 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building Animated Websites",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Takniik - Portfolio and Business Website",
+    des: "I developed a portfolio to effectively showcase the startup's offerings and establish industry credibility.",
+    img: "https://firebasestorage.googleapis.com/v0/b/portfolio-8cdcf.appspot.com/o/projects%2Ftakniik%2Fhero%20and%20service%20section.png?alt=media&token=27e17a0f-0c04-4e42-a2a9-f155f8583882",
+    iconLists: [
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-8cdcf.appspot.com/o/skills%2Freact.png?alt=media&token=179de42d-1430-4727-a35d-a5e68dca0c2c",
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-8cdcf.appspot.com/o/skills%2Fjs.png?alt=media&token=b9a18403-9614-46fe-a4b0-aa7b5c97659f",
+    ],
+    link: "https://takniik.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "TechBitCoin - Crypto Trading Web Portal",
+    des: "I developed a secure portal to trade TechBit Crypto, featuring a 3-level MLM referral system, optimized Coin payment gateway integration, and enhanced KYC verification for improved security and user engagement.",
+    img: "https://firebasestorage.googleapis.com/v0/b/portfolio-8cdcf.appspot.com/o/projects%2Ftechbitcrypto%2FDashboard.png?alt=media&token=8b10b2b3-a99e-45d3-abcc-b74fff75f7cd",
+    iconLists: [
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-8cdcf.appspot.com/o/skills%2Freact.png?alt=media&token=179de42d-1430-4727-a35d-a5e68dca0c2c",
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-8cdcf.appspot.com/o/skills%2Fspringboot.png?alt=media&token=7d6fafed-60c4-4b89-8cc1-1c013b213851",
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-8cdcf.appspot.com/o/skills%2Fkubernetes.png?alt=media&token=df752dca-6dbb-467d-9cae-d97e8fcb5eb9",
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-8cdcf.appspot.com/o/skills%2Fjava.png?alt=media&token=72829cd6-4ea3-4b73-aae9-5cea8013d149",
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-8cdcf.appspot.com/o/skills%2Fjs.png?alt=media&token=b9a18403-9614-46fe-a4b0-aa7b5c97659f",
+    ],
+    link: "https://www.techbitcrypto.com/",
+  },
+  {
+    id: 3,
+    title: "Kanbanboard Board - RESTful APIs",
+    des: "I developed a Kanban Board web portal to help teams manage, assign, track, and organize work efficiently.",
+    img: "https://firebasestorage.googleapis.com/v0/b/portfolio-8cdcf.appspot.com/o/projects%2Fkanban%2Fdashboard.png?alt=media&token=72fea23f-3de3-407d-90f3-827472702741",
+    iconLists: [
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-8cdcf.appspot.com/o/skills%2Freact.png?alt=media&token=179de42d-1430-4727-a35d-a5e68dca0c2c",
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-8cdcf.appspot.com/o/skills%2Fspringboot.png?alt=media&token=7d6fafed-60c4-4b89-8cc1-1c013b213851",
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-8cdcf.appspot.com/o/skills%2Fjava.png?alt=media&token=72829cd6-4ea3-4b73-aae9-5cea8013d149",
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-8cdcf.appspot.com/o/skills%2Fjs.png?alt=media&token=b9a18403-9614-46fe-a4b0-aa7b5c97659f",
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-8cdcf.appspot.com/o/skills%2Fkubernetes.png?alt=media&token=df752dca-6dbb-467d-9cae-d97e8fcb5eb9",
+    ],
+    link: "https://kanbanboard-web.netlify.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Sahil, an exceptional web developer, crafted a stunning personal portfolio, surpassing expectations with professionalism, expertise, and remarkable attention to detail. Highly recommended.",
+    name: "Mustaquim Khan",
+    title: "Civil Engineer at Khan Construction",
+    img: "https://firebasestorage.googleapis.com/v0/b/portfolio-8cdcf.appspot.com/o/testimonials%2Fmustaquim.jpeg?alt=media&token=33952fe3-c4af-42e6-b4a6-59f149daf7cc",
+  },
+  {
+    quote:
+      "Your work on our website exceeded expectations. Your professionalism, creativity, and responsiveness made the process smooth and enjoyable. We're delighted with the outcome and would recommend you without hesitation. Thank you, and we look forward to future collaborations.",
+    name: "Devanshu Kumar",
+    title: "Lead of Operations at Takniik",
+    img: "https://firebasestorage.googleapis.com/v0/b/portfolio-8cdcf.appspot.com/o/testimonials%2Fdevanshu.jpg?alt=media&token=ec8468d7-f845-44b9-9856-df3407241e49",
+  },
+  {
+    quote:
+      "Amazing job on our 3D website. Your work, dedication, creativity, and execution exceeded expectations. Thank you for your invaluable contribution.",
+    name: "Mr. Mike Morse",
+    title: "Managing Head, Marketing Company",
+    img: "https://firebasestorage.googleapis.com/v0/b/portfolio-8cdcf.appspot.com/o/testimonials%2Fuser-icon.png?alt=media&token=bcc4e6a7-8a5f-4504-b69e-75a359a4ac26",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Freelance Frontend Developer",
+    desc: "Designed and developed web app and websites for agencies React Js.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance Full Stack Developer",
+    desc: "Led the dev of a web app for a client, from initial concept to deployment on production environment.",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Software Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/sahilsheikh-dev",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://twitter.com/sahilsheikh_dev",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/sahilsheikh-dev",
+  },
+];
